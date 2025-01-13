d5e78956748b0821956f8d1cc3be8bf8bafe0bd0
sasaco <sasaicco@gmail.com>
2025-01-14 01:14:49 +0900

    できたー

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#507e32',        // ボタン濃い
+        'primary-light': '#99c184', // ボタン薄い
+        'disabled-bg': '#808080',   // 無効なINPUTの背景色
+        'border-green': '#70ad47',  // ボーダーライン
+        'nav-text': 'khaki',
+        'alert': '#aa0000',        // Alert text color
+        'input-border': '#767676'  // Input border color
+      }
+    }
+  },
+  plugins: [],
+}
+
