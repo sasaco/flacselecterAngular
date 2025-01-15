db9ac2d21ddd50010dd89b2c41ed4aec8218275d
sasaco <sasaicco@gmail.com>
2025-01-15 20:00:31 +0900

    Angular18の書き方に修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,19 @@ import { Component } from '@angular/core';
 import { ElectronService } from './providers/electron.service';
 import { AppConfig } from '../environments/environment';
 import packageJson from '../../package.json';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-
-    standalone: false
+    standalone: true,
+    imports: [RouterModule]
 })
 export class AppComponent {
-  
   public version: string;
 
   constructor(public electronService: ElectronService) {
-
     this.version = packageJson.version;
-
     if (electronService.isElectron) {
       console.log('Mode electron');
       console.log('Electron ipcRenderer', electronService.ipcRenderer);
